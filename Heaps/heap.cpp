55913661f50ebfeeac82eb4dd7ab64d7bc726fe5
Dhruv <dhruvsingh2405@gmail.com>
2022-10-23 00:13:53 +0530

    heaps

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -37,7 +37,57 @@ class heap {
         }
         cout<<endl;
     }
+
+    void deletefromHeap() {
+        if(size ==0){
+            cout<<"Nothing to delete"<<endl;
+            return;
+        }
+        // step 1: put last element to first element
+        arr[1] = arr[size];
+        // step 2: remove last element
+        size--;
+
+
+        // step 3: take root node to its correct position
+        int i = 1;
+        while(i<size){
+            int leftIndex = 2*i;
+            int rightIndex =2*i +1;
+
+            if(leftIndex < size && arr[i]< arr[leftIndex])
+            {
+                swap(arr[i],arr[leftIndex]);
+                i= leftIndex;
+            }
+            else if(rightIndex< size && arr[i]<arr[rightIndex]){
+                swap(arr[i],arr[rightIndex]);
+                i = rightIndex;
+            }
+            else{
+                return;
+            }
+        }
+    }
 };
+
+void heapify(int arr[], int n, int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i +1;
+
+    if(left<n && arr[largest]< arr[left]){
+        largest = left;
+    }
+    if(right<n && arr[largest]< arr[right]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
 
 int main() {
    heap h;
@@ -47,6 +97,19 @@ int main() {
    h.insert(52);
    h.insert(54);
    h.print();
+
+   h.deletefromHeap();
+   h.print();
+
+   int arr[6] = {-1, 54, 53,55,52,50};
+   int n =5;
+   for(int i = n/2; i>0;i--){
+    heapify(arr,n,i);
+   }
+   cout<<"printing the array now "<<endl;
+   for(int i=1; i<=n;i++){
+    cout<<arr[i]<<" ";
+   } cout<<endl;
 
    return 0;
 }
